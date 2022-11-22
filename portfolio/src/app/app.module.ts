d7a1de3723a8766d3f8ac8518faf87eb82b05a09
Josh W Mahnesmith <jwm2166@gmail.com>
2022-11-21 19:20:50 -0600

    Updated progress to the home screen

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/container/portfolio/portfolio.component';
+import { NavbarComponent } from './portfolio/component/navbar/navbar.component';
+import { NameCardComponent } from './portfolio/component/name-card/name-card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AvatarComponent } from './portfolio/component/avatar/avatar.component';
+import { HomeContactComponent } from './portfolio/component/home-contact/home-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    PortfolioComponent, NavbarComponent, NameCardComponent, AvatarComponent, HomeContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
